7d98a17c49c3bfcb1a6b6492e34363a152a57fa9
KhaffouKhaffou Mohamed <mohamed.khaffou-khaffou4@etu.univ-lorraine.fr>
2023-04-13 21:49:36 +0200

    fin tests

--- src/Tests/src/tests/classPortefeuille.test.ts
+++ src/Tests/src/tests/classPortefeuille.test.ts
@@ -63,3 +63,31 @@ test('Retranch monnaie non existante', () => {
     const m = new Monnaie('USD', 222, 2);
     expect(() => p.retrancheMonnaie(m)).toThrowError();
 });
+
+test('Get Total en euros', () => {
+    const p = new Portefeuille();
+    const m = new Monnaie('USD', 222, 2);
+    const m1 = new Monnaie('EUR', 300, 1);
+    p.ajouteMonnaie(m);
+    p.ajouteMonnaie(m1);
+    expect(p.getTotal()).toBe(411);
+});
+
+test('Get Total en euros vide', () => {
+    const p = new Portefeuille();
+    expect(p.getTotal()).toBe(0);
+});
+
+test('To string', () => {
+    const p = new Portefeuille();
+    const m = new Monnaie('USD', 222, 2);
+    const m1 = new Monnaie('EUR', 300, 1);
+    p.ajouteMonnaie(m);
+    p.ajouteMonnaie(m1);
+    expect(p.toString()).toBe('222 USD (111 euros) ; 300 EUR.');
+});
+
+test('To string vide', () => {
+    const p = new Portefeuille();
+    expect(p.toString()).toBe('Portefeuille vide...');
+});
